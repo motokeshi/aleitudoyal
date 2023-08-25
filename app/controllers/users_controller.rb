b4c47dd351125f47a2d7b53298d0b712a3693032
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-25 18:16:15 +0900

    ユーザー情報編集機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :message, :text)
+  end
 end
