0426da7736956a925bf038381b16846ed6508e3a
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-25 23:01:29 +0900

    サムネイル削除機能、ユーザー物理削除機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    if params[:thumb]
+      @user.image.purge
+      redirect_to user_path(@user)
+    else
+      @user.destroy
+      redirect_to root_path
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:image, :name, :message, :genre_id, :text)
