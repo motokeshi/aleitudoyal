8110bb1b9a30ac5b52141b1df3d02467c43bffd1
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-28 17:36:39 +0900

    アソシエーションを定義

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,2 +1,7 @@
 class Reminder < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image, dependent: :destroy
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
 end
