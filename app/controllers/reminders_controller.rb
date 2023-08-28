40aad909ab8f5a89b1138faf4ad1e2a9339a4207
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-28 18:30:07 +0900

    リマインダー新規登録機能を実装

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -4,5 +4,17 @@ class RemindersController < ApplicationController
   end
 
   def create
+    @reminder = Reminder.new(reminder_params)
+    if @reminder.save
+      @user = current_user
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def reminder_params
+    params.require(:reminder).permit(:image, :title, :genre_id, :outline, :frequency_year, :frequency_month, :frequency_week, :frequency_day).merge(user_id: params[:user_id])
   end
 end
