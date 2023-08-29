37b80190ddaba21aae4739a8fbb7e9f1c73d3d2b
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-29 13:36:27 +0900

    ルーティングのネストを解消

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
-  resources :users, only: [:show, :edit, :update, :destroy] do
-    resources :reminders
-  end
+  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :reminders
 end
