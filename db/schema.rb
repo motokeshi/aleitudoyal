588a86c6c0f712e2dfd1f80e68acd4570973b417
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-29 13:45:13 +0900

    recordモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_072132) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_043853) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_072132) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "records", charset: "utf8", force: :cascade do |t|
+    t.date "implementation_record", null: false
+    t.bigint "reminder_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reminder_id"], name: "index_records_on_reminder_id"
   end
 
   create_table "reminders", charset: "utf8", force: :cascade do |t|
@@ -73,5 +81,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_072132) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "records", "reminders"
   add_foreign_key "reminders", "users"
 end
