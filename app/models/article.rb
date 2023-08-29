f6790454acb4a6ba3b742c7b45df398003672708
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-29 22:50:17 +0900

    postモデルを作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :posts, dependent: :destroy
   has_one_attached :image, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_123511) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_134641) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,6 +47,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_123511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.text "text"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_posts_on_article_id"
   end
 
   create_table "records", charset: "utf8", force: :cascade do |t|
