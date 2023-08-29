0c06cb75a8c990d60e6f1f7ede4dc9de9f94cf91
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-29 18:12:43 +0900

    リマインダーのスキップ機能を実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,8 +1,12 @@
 class RecordsController < ApplicationController
   def create
     reminder = Reminder.find(params[:reminder_id])
-    record = Record.create(implementation_record: Date.today, reminder_id: reminder.id)
-    new_schedule = reminder.next_schedule_calculation(record.implementation_record, reminder.frequency_year, reminder.frequency_month, reminder.frequency_week, reminder.frequency_day)
+    if params[:skip]
+      new_schedule = reminder.next_schedule_calculation(reminder.schedule, reminder.frequency_year, reminder.frequency_month, reminder.frequency_week, reminder.frequency_day)
+    else
+      record = Record.create(implementation_record: Date.today, reminder_id: reminder.id)
+      new_schedule = reminder.next_schedule_calculation(record.implementation_record, reminder.frequency_year, reminder.frequency_month, reminder.frequency_week, reminder.frequency_day)
+    end
     reminder.update(schedule: new_schedule)
     if params[:root]
       redirect_to root_path
