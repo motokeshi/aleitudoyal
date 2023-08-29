d0b284203ecf6b802c10019b2af419f5dd0e5c87
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-29 16:32:20 +0900

    recordモデルを作成

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,6 @@
 class Reminder < ApplicationRecord
   belongs_to :user
+  has_many :records
   has_one_attached :image, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
