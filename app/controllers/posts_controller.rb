7a4dee0c52df66dbf1bc6bf7211fcc81ac083c3b
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-30 14:51:01 +0900

    postの編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,31 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(params.require(:post).permit(:image, :text).merge(article_id: params[:article_id]))
+    post = Post.new(post_params)
     if post.save
       redirect_to new_article_post_path(params[:article_id])
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @article = Article.find(params[:article_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to article_path(params[:article_id])
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :text).merge(article_id: params[:article_id])
   end
 end
