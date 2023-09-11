fa2c92efd1b44e55b1e9d3caca1720c2a3dd6c95
motokeshi <endless.jazz.waltz@gmail.com>
2023-09-11 17:05:56 +0900

    テストコードを修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/testimg.jpg'), filename: 'test_img.jpg')
     end
-    association :user
     association :article
   end
 end
