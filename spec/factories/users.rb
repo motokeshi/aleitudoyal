50a83ec54bea971dd4eb5102b36a6aebb971850c
motokeshi <endless.jazz.waltz@gmail.com>
2023-09-11 13:40:19 +0900

    userモデルのFactorybotの記述を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name    { Faker::Name }
+    name    { Faker::Name.initials(number: 2) }
     email   { Faker::Internet.email}
     password  { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
