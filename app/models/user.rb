164eb9aed757980f2ed55cd5ad5e629888a21f85
motokeshi <endless.jazz.waltz@gmail.com>
2023-09-20 17:43:16 +0900

    テストコードを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: true, length: { maximum: 40 }
-  # deviseの機能を使って、ユーザー更新ページを実装する。
-  # ユーザー登録時のパスワード欄に半角英数以外の文字をコピペして登録できてしまう。
-  # パスワードにバリデーションを設定すると、現状ではユーザー情報が更新できなくなってしまう。
   validates :password, format: { with: /\A[a-z\d]+\z/i }, on: :create
 
   has_many :reminders, dependent: :destroy
